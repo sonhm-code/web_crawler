97f361a0536e8aec99b099edc53774af093526f0
sonhm <>
2021-04-03 13:57:48 +0000

    add scrap code

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello world')
+from scrap import ask, scrap
+from babel.numbers import format_currency
+
+countries = scrap()
+print("Hello! Please choose select a country by number:")
+for index, country in enumerate(countries):
+  print(f"#{index} {country['name']}")
+ask()
