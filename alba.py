b7f3824b0ffce8c9179c57cf1afa2d601ed2dd07
sonhm <>
2021-04-05 15:11:59 +0000

    fix alba code

--- alba.py
+++ alba.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 os.system("clear")
 alba_url = "http://www.alba.co.kr"
 
+maximum_page = 3 # user customized range
 request = requests.get(alba_url)
 soup = BeautifulSoup(request.text,'html.parser')
 
@@ -14,12 +15,17 @@ super_brands = super_brands.find_all('a',attrs={"class":"goodsBox-info"})
 companies = []
 
 for super_brand in super_brands:
-  link = super_brand.find('a')
-  name = super_brand.span['company']
+  link = super_brand.get('href')
+  name = super_brand.find('span',attrs={'class':'company'}).string
   company = {
-    'link' : link,
+    'link' : f'{alba_url}{link}',
     'name' : name
   }
   companies.append(company)
+  
+customized_companies = companies[0:maximum_page]
 
-print(companies)
+for company in customized_companies:
+  company_url = company.get('link')
+  print(requests.get(company_url).status_code)
+  
