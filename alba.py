5bb2a3f77c1f6cfe69224677a448c8a353464509
sonhm <>
2021-04-06 03:34:04 +0000

    init

--- alba.py
+++ alba.py
@@ -3,23 +3,31 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-os.system("clear")
-alba_url = "http://www.alba.co.kr"
+def alba():
 
-request = requests.get(alba_url)
-soup = BeautifulSoup(request.text,'html.parser')
+  os.system("clear")
+  alba_url = "http://www.alba.co.kr"
 
-super_brands = soup.find('ul',attrs={"class":"goodsBox"})
-super_brands = super_brands.find_all('a',attrs={"class":"goodsBox-info"})
-companies = []
+  maximum_page = 3 # user customized range
+  request = requests.get(alba_url)
+  soup = BeautifulSoup(request.text,'html.parser')
 
-for super_brand in super_brands:
-  link = super_brand.find('a')
-  name = super_brand.span['company']
-  company = {
-    'link' : link,
-    'name' : name
-  }
-  companies.append(company)
+  super_brands = soup.find('ul',attrs={"class":"goodsBox"})
+  super_brands = super_brands.find_all('a',attrs={"class":"goodsBox-info"})
+  companies = []
 
-print(companies)
+  for super_brand in super_brands:
+    link = super_brand.get('href')
+    name = super_brand.find('span',attrs={'class':'company'}).string
+    company = {
+      'link' : f'{alba_url}{link}',
+      'name' : name
+    }
+    companies.append(company)
+    
+  customized_companies = companies[0:maximum_page]
+
+  for company in customized_companies:
+    company_url = company.get('link')
+    print(requests.get(company_url).status_code)
+    
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-hello
+from alba import alba
+
+alba()
